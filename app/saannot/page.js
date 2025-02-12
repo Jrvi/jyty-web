628ab5f7b86f68c64d8c2b421f55cce89236080a
Juho Järvi <juho.jarvi@solteq.com>
2025-02-12 17:41:28 +0200

    chore: added content to säännöt page

--- app/saannot/page.js
+++ app/saannot/page.js
@@ -1,5 +1,179 @@
 import React from 'react';
 
 export default function Page() {
-    return <h1>Hello, Next.js.</h1>
-  }
+  return (
+    <div>
+      <h1>Säännöt</h1>
+
+
+        <h2>1§ Yhdistyksen nimi ja kotipaikka</h2>
+
+        <p>
+          Yhdistyksen nimi on Jyväskylän teekkariyhdistys ry ja sen kotipaikka on Jyväskylä.
+        </p>
+
+        <h2>
+          2§ Yhdistyksen kieli
+        </h2>
+
+        <p>
+          Yhdistyksen virallinen kieli on suomi.
+        </p>
+
+        <h2>
+          3§ Yhdistyksen tarkoitus
+        </h2>
+
+        <p>
+          Yhdistys toimii Jyväskylän yliopiston teknistieteellisten opiskelijoiden yhdyssiteenä. Yhdistyksen tarkoituksena on edistää jäsenjärjestöjen ja henkilöjäsenten yhteiskunnallisia, ammatillisia, sosiaalisia ja henkisiä sekä opintoihin ja opiskeluun liittyviä pyrkimyksiä sekä vaalia ja kehittää jyväskyläläistä teekkariperinteitä ja -kulttuuria.
+        </p>
+
+        <h2>
+          4§ Tarkoituksen toteuttaminen
+        </h2>
+
+        <p>
+          Tarkoituksen toteuttamiseksi yhdistys:
+          tukee jäsenjärjestöjen ja henkilöjäsenten toimintaa, joka edistää yllä mainittujen opiskelijoiden opiskelua, opintojen edellytyksiä tai tulevaa uraa
+          toimii edunvalvojana yhdessä jäsenjärjestöjen kanssa koulutus- ja sosiaalipoliittisissa asioissa
+          kohottaa jyväskyläläisen teknillistieteellisen koulutusalan ja sitä opiskelevien arvostusta ja tuntemusta yhteiskunnassamme
+          edistää jäsenjärjestöjen keskinäistä yhteistyötä sekä yhteistyötä muiden teknistieteellisen alan toimijoiden välillä
+          toimii tiedonvälittäjänä ja lisää keskustelua yliopistoa, ylioppilaskuntaa ja työelämää koskevissa asioissa
+          harjoittaa tiedotus-, koulutus- ja julkaisutoimintaa sekä suomeksi, että englanniksi
+          sekä voi tarvittaessa ryhtyä muihinkin yhdistyksen tarkoitusta edistäviin toimiin.
+        </p>
+
+        <h2>
+          5§ Toiminnan tukeminen
+        </h2>
+
+        <p>
+          Toimintansa tukemiseksi yhdistys voi ottaa vastaan avustuksia, lahjoituksia ja testamentteja, järjestää huvitilaisuuksia ja arpajaisia asianomaisilla luvilla, harjoittaa alaan liittyvää julkaisutoimintaa, tehdä talkootöitä. Yhdistys voi omistaa toiminnan kannalta tarpeellista kiinteää ja irtainta omaisuutta sekä osakkeita. Yhdistys voi harjoittaa yhdistyslain 5 §:n mukaista taloudellista toimintaa tarkoituksensa toteuttamiseksi, mutta ei hanki taloudellista etua tai ansiota toimintaansa osallistuville jäsenille.
+        </p>
+
+        <h2>
+          6§ Jäsenyys
+        </h2>
+
+        <p>
+          Yhdistyksen jäseniksi voivat liittyä jäsenjärjestöt ja henkilöjäsenet, jotka voivat olla joko varsinaisia jäseniä tai kannatusjäseniä.
+          Yhdistyksen varsinaisiksi jäseniksi voidaan hyväksyä Jyväskylän yliopiston teknistieteellisissä koulutusohjelmissa toimivat ainutkertaiset sisäänotolliset opiskelijoiden killat, kaikkien teknillisten koulutusohjelmien vaihto-opiskelijoiden edunvalvontajärjestöt sekä teknistieteellisten koulutusohjelmien opiskelijat henkilöjäseninä.
+          Hallituksen kokous hyväksyy varsinaiset jäsenet yksinkertaisella äänten enemmistöllä. Jäsenhakemuksen voi jättää kirjallisesti hallitukselle.
+          Yhdistyksen kannatusjäseneksi voidaan hyväksyä muita yhdistyksen toiminnasta kiinnostuneita yhteisöjä ja yksityishenkilöitä, jotka haluavat tukea yhdistyksen tarkoitusta ja toimintaa. Yhdistyksen hallitus hyväksyy kannatusjäsenet kirjallisten hakemusten perusteella.
+          Henkilöjäsenten jäsenmaksu maksetaan kahdeksan (8) vuoden välein. Jäsenen tulee erota tai hänet voidaan hallituksen päätöksellä erottaa, kun hänen opintonsa Jyväskylän yliopistossa päättyvät. Jäsen voidaan katsoa eronneeksi, mikäli hän ei ole maksanut jäsenmaksuaan 1 kuukauden kuluessa uuden jäsenmaksukauden alkamisen jälkeen. Jäsen voidaan erottaa yhdistyksen jäsenyydestä, jos jäsen on laiminlyönyt jäsenmaksun maksamisen, jos jäsen ei ole enää jäsenyyskriteerien perusteella jäsenkelpoinen tai jos jäsen on toiminnallaan aiheuttanut merkittävää haittaa yhdistykselle tai sen maineelle. Jäsenen erottamisesta päättää yhdistyksen hallitus enemmistö äänillä. Jäsenellä on oikeus erota yhdistyksestä ilmoittamalla siitä kirjallisesti hallitukselle tai sen puheenjohtajalle tai ilmoittamalla erosta yhdistyksen kokouksessa merkittäväksi pöytäkirjaan.
+          Jäsenjärjestön jäsenmaksu maksetaan yhden (1) vuoden välein. Jäsen voidaan katsoa eronneeksi, mikäli hän ei ole maksanut jäsenmaksuaan 1 kuukauden kuluessa uuden jäsenmaksukauden alkamisen jälkeen. Jäsenjärjestön tulee erota tai jäsenjärjestö voidaan erottaa yhdistyksen jäsenyydestä, jos jäsenjärjestö on laiminlyönyt jäsenmaksun maksamisen, jos jäsenjärjestö ei ole enää jäsenyyskriteerien perusteella jäsenkelpoinen tai jos jäsenjärjestö on toiminnallaan aiheuttanut merkittävää haittaa yhdistykselle tai sen maineelle. Jäsenjärjestön erottamisesta päättää yhdistyksen hallitus enemmistö äänillä. Jäsenjärjestöllä on oikeus erota yhdistyksestä ilmoittamalla siitä kirjallisesti hallitukselle tai sen puheenjohtajalle tai ilmoittamalla erosta yhdistyksen kokouksessa merkittäväksi pöytäkirjaan.
+        </p>
+
+        <h2>
+          7§ Varsinaiset kokoukset
+        </h2>
+
+        <p>
+          Yhdistys pitää vuosittain kaksi varsinaista kokousta. Yhdistyksen kevätkokous pidetään tammi-toukokuussa ja syyskokous syys-joulukuussa hallituksen määräämänä päivänä.
+
+          Hallituksen on kutsuttava yhdistyksen kokoukset koolle vähintään seitsemän vuorokautta ennen kokousta sähköpostitse jäsenen ilmoittamaan osoitteeseen sekä jäsenille tarkoitetulla some-kanavalla.
+
+          Yhdistyksen kevätkokouksessa päätetään seuraavat asiat:
+          esitetään tilinpäätös, toimintakertomus ja toiminnantarkastajan lausunto
+          päätetään tilinpäätöksen vahvistamisesta ja vastuuvapauden myöntämisestä hallitukselle ja muille tili- ja vastuuvelvollisille
+          Yhdistyksen syyskokouksessa päätetään seuraavat asiat:
+          vahvistetaan toimintasuunnitelma sekä talousarvio
+          vahvistetaan ohjesäännöt ja muut tarvittavat säännökset seuraavalle vuodelle
+          valitaan hallituksen puheenjohtaja
+          valitaan hallituksen muut jäsenet
+          valitaan yksi tai kaksi toiminnantarkastajaa ja heille yksi tai kaksi varatoiminnantarkastajaa
+          päätetään varsinaisilta jäseniltä ja kannattavilta jäseniltä perittävän liittymis- ja jäsenmaksun suuruus
+          Mikäli yhdistyksen jäsen haluaa saada jonkin asian yhdistyksen kevät- tai syyskokouksen käsiteltäväksi, on hänen ilmoitettava siitä kirjallisesti hallitukselle niin hyvissä ajoin, että asia voidaan sisällyttää kokouskutsuun.
+          Henkilöjäsenellä on yksi (1) ääni yhdistyksen kokouksissa ja Jäsenjärjestöllä on yksi (1) ääni jokaista alkavaa viittäkymmentä (50) jäsentä kohden.
+        </p>
+
+        <h2>
+          8§ Yhdistyksen hallitus
+        </h2>
+
+        <p>
+          Yhdistyksen asioita hoitaa yhdistyksen syyskokouksessa valittu hallitus, jonka toimikausi on kalenterivuosi. Yhdistyksen hallitukseen kuuluu syyskokouksessa valitut puheenjohtaja ja 2-12 muuta varsinaista jäsentä. Hallituksen jäsenen tulee olla valintahetkellä yhdistyksen jäsenjärjestön varsinainen jäsen tai yhdistyksen henkilöjäsen. Hallitus voi ottaa keskuudestaan tai ulkopuoleltaan yhdistykselle tarpeellisia toimihenkilöitä.
+
+          Hallitus kokoontuu puheenjohtajan tai varapuheenjohtajan kutsusta, kun he katsovat siihen olevan aihetta, tai kun vähintään kolme (3) hallituksen jäsentä sitä vaatii. Hallituksen kokoukset ovat päätösvaltaisia, jos kokouksesta on ilmoitettu hallituksen jäsenille vähintään 3 päivää ennen kokousta, ja kokouksessa ovat läsnä puheenjohtaja tai varapuheenjohtaja sekä vähintään puolet (1/2) hallituksen jäsenistä.
+        </p>
+
+        <h2>
+          9§ Hallituksen tehtävät
+        </h2>
+
+        <p>
+          Hallituksen tehtävä on:
+          johtaa yhdistyksen toimintaa yhdistyksen sääntöjen ja päätösten mukaan sekä edustaa yhdistystä
+          huolehtia yhdistyksen taloudesta ja hallinnosta
+          valmistella yhdistyksen kokouksissa käsiteltävät asiat
+          laatia yhdistyksen toimintasuunnitelma ja -kertomus sekä talousarvio ja tilinpäätös
+          valita tarvittavat toimihenkilöt ja työryhmät ja valvoa niiden toimintaa
+          vastata lakinkäyttölupien myöntämisestä
+          vastata teekkarilakkiohjesäännöstä huomioiden jäsenten yhdenvertaisen ja tasapuolisen kohtelun
+          ylläpitää teekkarilakkitilauksia
+          tiedottaa jäsenjärjestöille ja henkilöjäsenille hallituksen päätöksiä
+        </p>
+
+        <h2>
+          10§ Päätöksentekojärjestys  ja vaalit
+        </h2>
+
+        <p>
+          Äänestyksissä käytetään avointa äänestystä, ellei yhdistyksen kokouksessa vähintään viisi (5) äänivaltaista edustajaa tai hallituksen kokouksissa vähintään kaksi (2) hallituksen jäsentä vaadi suljettua äänestystä. Jos käsiteltävästä asiasta on annettu useita päätösehdotuksia, noudatetaan kokouksen puheenjohtajan päättämää soveltuvaa äänestysjärjestystä. Kokouksen puheenjohtaja antaa äänestystä täsmentävät ohjeet ennen äänestyksen suorittamista. Yhdistyksen kokouksissa ja hallituksen kokouksissa äänestykset ratkaistaan yksinkertaisella äänten enemmistöllä. Äänestyksen mennessä tasan ratkaisee puheenjohtajan kanta, mutta henkilövaaleissa kuitenkin arpa. Mikäli ensimmäisellä kierroksella kukaan ei ole saanut ehdotonta äänten enemmistöä, suoritetaan uusi vaali kahden (2) ensimmäisessä äänestyksessä eniten ääniä saaneen ehdokkaan välillä. Jos on valittavana useita henkilöitä, toimitetaan vaali siirtoääniperiaatteella toimitettavana listavaalina.
+        </p>
+
+        <h2>
+          11§ Yhdistyksen nimen kirjoittaminen
+        </h2>
+
+        <p>
+          Yhdistyksen nimen kirjoittaa yhdistyksen hallituksen puheenjohtaja yksin tai varapuheenjohtaja yksin. Oikeus yhdistyksen nimen kirjoittamiseen on lisäksi henkilöllä, jolla on siihen hallituksen erikseen antama henkilökohtainen oikeus.
+        </p>
+
+        <h2>
+          12§ Tilikausi, toiminnantarkastajat
+        </h2>
+
+        <p>
+          Yhdistyksen tilikausi on kalenterivuosi.
+
+          Yhdistyksen hallintoa ja taloutta valvoo yksi tai kaksi (1-2) toiminnantarkastajaa. Toiminnantarkastajien toimikausi on kalenterivuosi. Toiminnantarkastajilla on läsnäolo- ja puheoikeus yhdistyksen ja hallituksen kokouksissa, jossa käsitellään heidän tehtäviinsä liittyviä asioita. Toiminnantarkastajilla on oikeus halutessaan tutustua yhdistyksen talouteen ja hallintoon.
+
+          Tilinpäätös sekä yhdistyksen kirjanpito- ja hallintoasiakirjat on annettava toiminnantarkastajille vähintään neljä (4) viikkoa ennen yhdistyksen kevätkokousta. Toiminnantarkastajien tulee antaa kirjallinen lausuntonsa viimeistään kahta (2) viikkoa ennen kokousta.
+        </p>
+
+        <h2>
+          13§ Sääntöjen muuttaminen
+        </h2>
+
+        <p>
+          Päätös sääntöjen muuttamisesta on tehtävä yhdistyksen kokouksessa. Esitystä on puollettava vähintään kolme neljäsosaa (3/4) annetuista äänistä. Sääntömuutoksesta on ilmoitettava kokouskutsussa.
+        </p>
+
+        <h2>
+          14§ Yhdistyksen purkaminen
+        </h2>
+
+        <p>
+          Päätös yhdistyksen purkamisesta on tehtävä kahdessa peräkkäisessä yhdistyksen kokouksessa, jotka on pidettävä vähintään kahden viikon välein. Esitystä on puollettava vähintään kolme neljäsosaa (3/4) annetuista äänistä.
+        </p>
+
+        <h2>
+          15§ Yhdistyksen varojen käyttö sen purkautuessa
+        </h2>
+
+        <p>
+          Yhdistyksen purkautuessa varat käytetään yhdistyksen tarkoituksen edistämiseen purkamisesta päättävän kokouksen määräämällä tavalla, esimerkiksi luovuttamalla ne yhdistyksen kanssa samoja tarkoitusperiä ajavalle rekisteröidylle yhdistykselle, julkisyhteisölle tai säätiölle.
+        </p>
+
+        <h2>
+          16§ Voimaantulo
+        </h2>
+
+        <p>
+          Sääntömuutos tai purkautumispäätös tulee voimaan, kun se on merkitty yhdistysrekisteriin.
+        </p>
+
+    </div>
+  )
+}
