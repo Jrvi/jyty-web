1b2f66b4522dbfeb0cdd7ddf86c9851d3bddac31
Juho Järvi <juho.jaervi@gmail.com>
2025-02-13 10:37:24 +0200

    chore: added content to killat

---
+++ app/killat/algo/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Algo ry</h1>
+            <p>Algo ry on Jyväskylän ensimmäinen teekkari kilta, joka perustettiin vuonna 2022.</p>
+            <p>Algo ry jäsenistöön kuuluu tieto- ja ohjelmistotekniikan opiskelijoita ja teknologiajohtamisen opiskelijoita</p>
+            <a href="https://www.algojkl.com/">Nettisivut</a>
+        </div>
+    )
+}
