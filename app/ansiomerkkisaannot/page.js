90a91b541424882550ff1b437973aecd8867f3fa
Juho Järvi <juho.jaervi@gmail.com>
2025-02-18 10:49:41 +0200

    chore: added material ui

---
+++ app/ansiomerkkisaannot/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div className='container'>
+            <h1>Ansiomerkki säännöt</h1>
+            <p>Tähän tuodaan ansiomerkki säännöt</p>
+        </div>
+    );
+}
---
+++ app/lakkisaannot/page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div className='container'>
+            <h1>Teekkarilakki säännöt</h1>
+            <p>Tähän teekkarilakki säännöt</p>
+        </div>
+    );
+}
